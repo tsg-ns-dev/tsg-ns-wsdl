2bc34feefc5e33eb5abbdc382e4416bc643c1c25
Kim Jones <kpegues@techsoup.org>
2023-09-14 17:24:07 -0500

    version 2023.1 updates

--- src/main/java/tsg/ns/wsdl/coop/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * 		Release Status: Unreleased
- * 	
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:accounting_2022_2.lists.webservices.netsuite.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package tsg.ns.wsdl.coop;
